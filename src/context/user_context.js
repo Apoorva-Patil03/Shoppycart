7126427d9cf897cd756cbebee1ca3a27764aa38f
Apoorva Patil <apoorvapatil0347@gmail.com>
2025-12-26 11:42:22 +0530

    Refactor UserContext to App component structure

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -1,22 +1,42 @@
-import React, { useContext, useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-const UserContext = React.createContext();
-export const UserProvider = ({ children }) => {
-  const { loginWithRedirect, logout, user } = useAuth0();
+function App() {
+  const {
+    isLoading, // Loading state, the SDK needs to reach Auth0 on load
+    isAuthenticated,
+    error,
+    loginWithRedirect: login, // Starts the login flow
+    logout: auth0Logout, // Starts the logout flow
+    user, // User profile
+  } = useAuth0();
 
-  const [myUser, setMyUser] = useState(null);
+  const signup = () =>
+    login({ authorizationParams: { screen_hint: "signup" } });
 
-  useEffect(() => {
-    setMyUser(user);
-  }, [user]);
+  const logout = () =>
+    auth0Logout({ logoutParams: { returnTo: window.location.origin } });
 
-  return (
-    <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
-      {children}
-    </UserContext.Provider>
+  if (isLoading) return "Loading...";
+
+  return isAuthenticated ? (
+    <>
+      <p>Logged in as {user.email}</p>
+
+      <h1>User Profile</h1>
+
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <button onClick={logout}>Logout</button>
+    </>
+  ) : (
+    <>
+      {error && <p>Error: {error.message}</p>}
+
+      <button onClick={signup}>Signup</button>
+
+      <button onClick={login}>Login</button>
+    </>
   );
-};
-export const useUserContext = () => {
-  return useContext(UserContext);
-};
+}
+
+export default App;
